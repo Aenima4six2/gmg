45779768dea406057eb128722127f9692913edb1
John-Paul Dienst <2087362+Aenima4six2@users.noreply.github.com>
2017-09-04 16:46:42 -0400

    disable sass

--- server/app.js
+++ server/app.js
@@ -16,12 +16,12 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}))
+// app.use(sassMiddleware({
+//   src: path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   indentedSyntax: true, // true = .sass and false = .scss
+//   sourceMap: true
+// }))
 
 // Static content
 app.use(express.static(path.join(__dirname, 'public')))
